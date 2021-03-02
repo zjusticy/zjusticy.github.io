941abe58bde7970aa8a4301c47545601c6c6737c
zjusticy <stevens123321@outlook.com>
2021-03-01 17:28:11 -0800

    change footer style

---
+++ out/_next/static/webpack/static/development/pages/index.js.4d3b2807d845c8dd98c6.hot-update.js
@@ -0,0 +1,31 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js?!./node_modules/next/dist/compiled/postcss-loader/index.js?!./node_modules/resolve-url-loader/index.js?!./node_modules/sass-loader/dist/cjs.js?!./components/layout.module.scss":
+/*!*******************************************************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js??ref--5-oneOf-3-1!./node_modules/next/dist/compiled/postcss-loader??__nextjs_postcss!./node_modules/resolve-url-loader??ref--5-oneOf-3-3!./node_modules/sass-loader/dist/cjs.js??ref--5-oneOf-3-4!./components/layout.module.scss ***!
+  \*******************************************************************************************************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+// Imports
+var ___CSS_LOADER_API_IMPORT___ = __webpack_require__(/*! ../node_modules/css-loader/dist/runtime/api.js */ "./node_modules/css-loader/dist/runtime/api.js");
+exports = ___CSS_LOADER_API_IMPORT___(true);
+// Module
+exports.push([module.i, ".layout_myFooter__JubIR {\n  padding-bottom: 1.5rem;\n  padding-top: 1.5rem;\n  border-top: 1px solid rgba(0, 0, 0, 0.1);\n  font-size: 1rem;\n  padding-left: 0.75rem;\n  padding-right: 0.75rem; }\n\n.layout_lastColor__ALfcM {\n  color: rgba(0, 0, 0, 0.5);\n  padding-top: 0.5rem; }\n\n.layout_addBorder__2Y_EI {\n  border: 2px solid rgba(0, 0, 0, 0.5); }\n\n.layout_moreMargin__2KGfE {\n  padding: 0rem 0.5rem;\n  margin: 0.5rem 1rem; }\n\n.layout_navEndSettings__227S4 a:nth-child(3) {\n  margin-right: 0; }\n\n.layout_burgerExtra__1w06x {\n  padding-right: 0.75rem; }\n\n.layout_footerStyle__2iYeS {\n  display: flex; }\n", "",{"version":3,"sources":["/Users/zhutong/code/my-next-blog/components/layout.module.scss"],"names":[],"mappings":"AAAA;EACC,sBAAsB;EACtB,mBAAmB;EACnB,wCAAwC;EACxC,eAAe;EACf,qBAAqB;EACrB,sBAAsB,EAAA;;AAGvB;EACC,yBAAyB;EACzB,mBAAmB,EAAA;;AAGpB;EACC,oCAAoC,EAAA;;AAGrC;EACC,oBAAoB;EACpB,mBAAmB,EAAA;;AAGpB;EAEE,eAAe,EAAA;;AAIjB;EACC,sBAAsB,EAAA;;AAGvB;EACC,aAAa,EAAA","file":"layout.module.scss","sourcesContent":[".myFooter {\n\tpadding-bottom: 1.5rem;\n\tpadding-top: 1.5rem;\n\tborder-top: 1px solid rgba(0, 0, 0, 0.1);\n\tfont-size: 1rem;\n\tpadding-left: 0.75rem;\n\tpadding-right: 0.75rem;\n}\n\n.lastColor {\n\tcolor: rgba(0, 0, 0, 0.5);\n\tpadding-top: 0.5rem;\n}\n\n.addBorder {\n\tborder: 2px solid rgba(0, 0, 0, 0.5);\n}\n\n.moreMargin {\n\tpadding: 0rem 0.5rem;\n\tmargin: 0.5rem 1rem;\n}\n\n.navEndSettings {\n\t& a:nth-child(3) {\n\t\tmargin-right: 0;\n\t}\n}\n\n.burgerExtra {\n\tpadding-right: 0.75rem;\n}\n\n.footerStyle {\n\tdisplay: flex;\n}\n"]}]);
+// Exports
+exports.locals = {
+	"myFooter": "layout_myFooter__JubIR",
+	"lastColor": "layout_lastColor__ALfcM",
+	"addBorder": "layout_addBorder__2Y_EI",
+	"moreMargin": "layout_moreMargin__2KGfE",
+	"navEndSettings": "layout_navEndSettings__227S4",
+	"burgerExtra": "layout_burgerExtra__1w06x",
+	"footerStyle": "layout_footerStyle__2iYeS"
+};
+module.exports = exports;
+
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.4d3b2807d845c8dd98c6.hot-update.js.map
